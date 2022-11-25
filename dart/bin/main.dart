3dc0e6e8ea48ea5046ef0e88f6b7c5c2291ef1b6
anton.bashirov <anton.sh.local@gmail.com>
2022-11-25 14:32:09 +0300

    fix

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -6,30 +6,58 @@ import 'dart:isolate';
 import 'package:args/args.dart';
 import 'package:benchmark/storage/bindings.dart';
 
+int? parseInt(ArgParser parser, ArgResults parsed, String name, {bool optional = false}) {
+  final string = parsed[name];
+  if (string == null) {
+    if (optional) return null;
+    print("usage: ${parser.usage}");
+    exit(-1);
+  }
+  final value = int.tryParse(string);
+  if (value == null) {
+    if (optional) return null;
+    print("usage: ${parser.usage}");
+    exit(-1);
+  }
+  return value;
+}
+
 Future<void> main(List<String> args) async {
   final parser = ArgParser();
   parser.addOption("count", abbr: "c");
-  final parsed;
+  parser.addOption("isolates", abbr: "i", mandatory: false);
+  ArgResults parsed;
   try {
     parsed = parser.parse(args);
   } catch (exception) {
     print("usage: ${parser.usage}");
     exit(-1);
   }
-  final countString = parsed["count"];
-  if (countString == null) {
-    print("usage: ${parser.usage}");
-    exit(-1);
-  }
-  final count = int.tryParse(countString);
-  if (count == null) {
-    print("usage: ${parser.usage}");
-    exit(-1);
-  }
+
   final bindings = BenchmarkBindings(DynamicLibrary.open("${Directory.current.path}/bin/libbenchmark.so"));
   final completer = Completer();
   var element = 0;
   final port = ReceivePort();
+  final count = parseInt(parser, parsed, "count")!;
+  final isolates = parseInt(parser, parsed, "isolates", optional: true);
+  if (isolates != null) {
+    await Isolate.spawn<int>((int count) async {
+      final bindings = BenchmarkBindings(DynamicLibrary.open("${Directory.current.path}/bin/libbenchmark.so"));
+      final completer = Completer();
+      var element = 0;
+      final port = ReceivePort();
+      port.listen((message) {
+        if (++element >= count) completer.complete(null);
+      });
+      final stopwatch = Stopwatch();
+      stopwatch.start();
+      bindings.dart_run_benchmark(count, port.sendPort.nativePort);
+      await completer.future;
+      print("Benchmark finished at ${stopwatch.elapsed} seconds");
+      exit(0);
+    }, count ~/ isolates);
+    return;
+  }
   port.listen((message) {
     if (++element >= count) completer.complete(null);
   });
